4fe608374d929fb2d66b1426d3ee482cbd401e47
Noé Rivera <tsnoe.dali@gmail.com>
2023-02-26 22:18:32 -0600

    create database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,23 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::create([
+            'name' => 'Papas Fritas',
+            'description' => 'Papas Fritas',
+            'price' => 100.00,
+        ]);
+
+        Product::create([
+            'name' => 'Chocolate KitKat',
+            'description' => 'Delicioso Chocolate KitKat de 100 gramos',
+            'price' => 200.00,
+        ]);
+
+        Product::create([
+            'name' => 'Refresco Coca Cola',
+            'description' => 'El clásico refresco Coca Cola de 1.5 litros',
+            'price' => 300.00,
+        ]);
     }
 }
